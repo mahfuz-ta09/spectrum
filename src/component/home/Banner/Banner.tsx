f7aa33dff02a7fa16128e905a5ed24adb9da2a84
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-04-10 22:05:49 +0600

    late start1.1

--- src/component/home/Banner/Banner.tsx
+++ src/component/home/Banner/Banner.tsx
@@ -3,7 +3,11 @@ import '@/css/home/Banner/Banner.css';
 const Banner = () => {
   return (
     <div className='banner'>
-        {/* <img src="https://images.pexels.com/photos/4102615/pexels-photo-4102615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
+      <div className='banner__content'>
+        <h1 className='banner__title'>Welcome to Our Website</h1>
+        <p className='banner__description'>Discover amazing content and connect with others.</p>
+        <button className='banner__button'>Get Started</button>
+      </div>
     </div>
   )
 }
