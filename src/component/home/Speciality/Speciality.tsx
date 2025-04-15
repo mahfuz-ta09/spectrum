e80f3011bef57686ec77f9f2260ba0bb81b55f4a
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-04-15 15:52:16 +0600

    banner done/specialization1.1

--- src/component/home/Speciality/Speciality.tsx
+++ src/component/home/Speciality/Speciality.tsx
@@ -4,7 +4,74 @@ import '@/css/home/Speciality/Speciality.css';
 const Speciality = () => {
     return (
         <div className='speciality-container'>
-            Speciality
+            <div className="spacialty-contaier">
+                <h1>Our Specialities</h1>
+                <p>We are the best in the business</p>
+                
+                <div className="speciality-slider">
+                    <div className="speciality-cards">
+
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+                        <div className="speciality-card">
+                            <h1>5000+</h1>
+                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                            <p>active users</p>
+                        </div>
+
+
+                    </div>
+                </div>
+            
+            </div>
         </div>
     )
 }
