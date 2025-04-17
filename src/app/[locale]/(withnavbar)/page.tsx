143d2e7aa9fc2cba880dea288672aae473cbe581
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-04-18 03:55:14 +0600

    trying new1.6

--- src/app/[locale]/(withnavbar)/page.tsx
+++ src/app/[locale]/(withnavbar)/page.tsx
@@ -1,11 +1,15 @@
 import Banner from "@/component/home/Banner/Banner"
 import Courses from "@/component/home/Courses/Courses"
+import Speciality from "@/component/home/Speciality/Speciality"
+
+
 
 const Page = () => {
 
     return (
         <div className="page">
             <Banner />
+            <Speciality />
             <Courses />
         </div>
     )
--- src/component/home/Courses/Courses.tsx
+++ src/component/home/Courses/Courses.tsx
@@ -1,81 +1,93 @@
 import '@/css/home/Courses/Courses.css';
 
 
+
 const Courses = () => {
     return (
         <div className='home-courses'>
             <div className="home-courses-container">
+                
                 <div className="home-courses-header">
                     <h1>Courses</h1>
                     <p>Explore our courses</p>
                 </div>
                 
                 <div className="course-group">
-                    <button>BSC</button>
-                    <button>ARTS</button>
-                    <button>COMMERCE</button>
+                    <button className='depurtment-btn'>SCIENCE</button>
+                    <button className='depurtment-btn'>COMMERCE</button>
+                    <button className='depurtment-btn'>ARTS</button>
                 </div>
                 
                 <div className="course-title">
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
-                    <button>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>pdasdahysics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physicdfds</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
+                    <button className='course-title-btn'>physics</button>
                 </div>
 
+                <div className="course-years-container">
+                    <div className="course-years">
+                        <div className="years">
+                            <h1 className='title'><span>1st</span> year</h1>
+                            <div className="details">
+                                <p>Science</p>
+                                <p>Department of physics</p>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo eius laborum? Magni, qui et.</p>
+                            </div>
+                            <div className="rear-btn">
+                                <button className='rear-btn-1'>enroll</button>
+                                <button className='rear-btn-1'>details</button>
+                            </div>
+                        </div>
+                        <div className="years">
+                            <h1 className='title'><span>2nd</span> year</h1>
+                            <div className="details">
+                                <p>Science</p>
+                                <p>Department of physics</p>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo eius laborum? Magni, qui et.</p>
+                            </div>
+                            <div className="rear-btn">
+                                <button className='rear-btn-1'>enroll</button>
+                                <button className='rear-btn-1'>details</button>
+                            </div>
+                        </div>
+                        <div className="years">
+                            <h1 className='title'><span>3rd</span> year</h1>
+                            <div className="details">
+                                <p>Science</p>
+                                <p>Department of physics</p>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo eius laborum? Magni, qui et.</p>
+                            </div>
+                            <div className="rear-btn">
+                                <button className='rear-btn-1'>enroll</button>
+                                <button className='rear-btn-1'>details</button>
+                            </div>
+                        </div>
+                        <div className="years">
+                            <h1 className='title'><span>4th</span> year</h1>
+                            <div className="details">
+                                <p>Science</p>
+                                <p>Department of physics</p>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo eius laborum? Magni, qui et.</p>
+                            </div>
+                            <div className="rear-btn">
+                                <button className='rear-btn-1'>enroll</button>
+                                <button className='rear-btn-1'>details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
