8e20e3a04b0b4a7c13965a7ae1b6222e01a780ee
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-04-24 00:57:08 +0600

    specialty1.3

--- src/component/home/Speciality/Speciality.tsx
+++ src/component/home/Speciality/Speciality.tsx
@@ -9,12 +9,12 @@ const Speciality = () => {
         <div className='speciality-container'>
             <div className="specialty-section">
                 <div className='specialty-content'>
-                    
+
                     <div className="first-half">
                         <h1>Our Specialities</h1>
                         <p>
-                            We're building Bangladesh's  <span>most comprehensive academic ecosystem</span> 
-                            - where sharing knowledge rewards you. At <span>SPECTRUM</span>, we're transforming exam prep across Bangladesh's universities
+                            We&apos;re building Bangladesh&apos;s <span>most comprehensive academic ecosystem</span> 
+                            - where sharing knowledge rewards you. At <span>SPECTRUM</span>, we&apos;re transforming exam prep across Bangladesh&apos;s universities
                             with these key strengths:  
                         </p>
                         <img src="https://aiolympiad.xyz/img/ai-olympiad.png" alt="" />
