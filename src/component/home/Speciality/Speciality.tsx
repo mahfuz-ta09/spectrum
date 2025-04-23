d7576f7b109ddc103c937330f90d7041898b087c
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-04-24 00:50:49 +0600

    specialty

--- src/component/home/Speciality/Speciality.tsx
+++ src/component/home/Speciality/Speciality.tsx
@@ -1,76 +1,61 @@
 import '@/css/home/Speciality/Speciality.css';
+import { faBuilding, faLightbulb } from '@fortawesome/free-regular-svg-icons';
+import { faGraduationCap, faMoneyCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const Speciality = () => {
     return (
         <div className='speciality-container'>
-            <div className="spacialty-contaier">
-                <h1>Our Specialities</h1>
-                <p>We are the best in the business</p>
-                
-                <div className="speciality-slider">
-                    <div className="speciality-cards">
-
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        {/* <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div>
-                        <div className="speciality-card">
-                            <h1>5000+</h1>
-                            <img src="https://images.pexels.com/photos/30434991/pexels-photo-30434991/free-photo-of-black-and-white-highway-landscape-santiago-di-compostela.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                            <p>active users</p>
-                        </div> */}
-
-
+            <div className="specialty-section">
+                <div className='specialty-content'>
+                    
+                    <div className="first-half">
+                        <h1>Our Specialities</h1>
+                        <p>
+                            We're building Bangladesh's  <span>most comprehensive academic ecosystem</span> 
+                            - where sharing knowledge rewards you. At <span>SPECTRUM</span>, we're transforming exam prep across Bangladesh's universities
+                            with these key strengths:  
+                        </p>
+                        <img src="https://aiolympiad.xyz/img/ai-olympiad.png" alt="" />
                     </div>
+
+                    <div className="second-half">
+                        <div>
+                            <FontAwesomeIcon icon={faBuilding} className='icon' />
+                            <h1>Pan-University Resource Hub</h1>
+                            <p>
+                                The only platform covering all major universities - from (examples) 
+                                with verified course materials. 
+                            </p>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faGraduationCap} className='icon' />
+                            <h1>Expert-Verified Content</h1>
+                            <p>
+                                Every resource is validated by department toppers and alumni 
+                                for 100% accuracy. 
+                            </p>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faLightbulb} className='icon' />
+                            <h1>Smart AI Search</h1>
+                            <p>
+                                Pinpoint exactly what you need with university-specific filters
+                                (course codes, level/semesters, professor names).  
+                            </p>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faMoneyCheck} className='icon' />
+                            <h1>Earn While You Learn</h1>
+                            <p>
+                                Contribute your notes/papers to our crowdsourced library and 
+                                get paid for every 100 downloads of your uploads!  
+                            </p>
+                        </div>
+                    </div>
+
                 </div>
-            
             </div>
         </div>
     )
