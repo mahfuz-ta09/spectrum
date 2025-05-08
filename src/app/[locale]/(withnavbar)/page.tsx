7792075c5eb3eefa1ae0086c392052ea56243c39
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-05-09 00:06:13 +0600

    faq added

--- src/app/[locale]/(withnavbar)/page.tsx
+++ src/app/[locale]/(withnavbar)/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/component/Footer/Footer"
 import Banner from "@/component/home/Banner/Banner"
 import Courses from "@/component/home/Courses/Courses"
+import FAQ from "@/component/home/FAQ/FAQ"
 import Speciality from "@/component/home/Speciality/Speciality"
 import WhyChooseUs from "@/component/home/WhyChooseUs/WhyChooseUs"
 
@@ -14,6 +15,7 @@ const Page = () => {
             <Speciality />
             <Courses />
             <WhyChooseUs />
+            <FAQ />
             <Footer />
         </div>
     )
---
+++ src/component/home/FAQ/FAQ.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import '@/css/home/FAQ/FAQ.css';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect } from 'react';
+
+
+const FAQ = () => {
+
+
+    return (
+        <div className="faq">
+            <div className="faq-content">
+                <div className="faq-header">
+                    <h1 className='faq-header-que'>How can we help you?</h1>
+                    <p className='faq-header-det'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus numquam. Ea dicta praesentium accusantium possimus consequuntur eos tenetur quibusdam.</p>
+                    <button className='faq-ask-que'>ask something else</button>
+                </div>
+
+                <div className="faq-question">
+
+                    <div className="question">
+                        <input type="checkbox" name="accordion" id="question1" className="question-checkbox" hidden />
+                        
+                        <label htmlFor="question1" className="question-header">
+                            <span className="head-question">Lorem ipsum dolor sit amet.</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="plus-icon" />
+                        </label>
+
+                        <div className="question-body">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias
+                            eum esse, dolore maxime voluptate iste.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="question">
+                        <input type="checkbox" name="accordion" id="question2" className="question-checkbox" hidden />
+                        
+                        <label htmlFor="question2" className="question-header">
+                            <span className="head-question">Lorem ipsum dolor sit amet.</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="plus-icon" />
+                        </label>
+
+                        <div className="question-body">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias
+                            eum esse, dolore maxime voluptate iste.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="question">
+                        <input type="checkbox"  name="accordion" id="question3" className="question-checkbox" hidden />
+                        
+                        <label htmlFor="question3" className="question-header">
+                            <span className="head-question">Lorem ipsum dolor sit amet.</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="plus-icon" />
+                        </label>
+
+                        <div className="question-body">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias
+                            eum esse, dolore maxime voluptate iste.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="question">
+                        <input type="checkbox"  name="accordion" id="question4" className="question-checkbox" hidden />
+                        
+                        <label htmlFor="question4" className="question-header">
+                            <span className="head-question">Lorem ipsum dolor sit amet.</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="plus-icon" />
+                        </label>
+
+                        <div className="question-body">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias
+                            eum esse, dolore maxime voluptate iste.
+                            </p>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ
