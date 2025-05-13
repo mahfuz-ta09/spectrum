2a576e561d790ef94cfa87cd3e837bc792b4fc19
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-05-14 00:56:40 +0600

    Cookie system changed1.1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { cookies } from 'next/headers';
 import {  NextRequest } from 'next/server';
 import { jwtDecode } from 'jwt-decode'
 import createIntlMiddleware from 'next-intl/middleware';
